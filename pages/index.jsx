ab4cc639728563b91797e7a2a6707c612fa8e080
avo-ss <anton.vo@soprasteria.com>
2022-05-18 13:32:50 +0200

    add presented date to index

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,12 @@ import TailwindDark from '../public/svgs/tw-logo-dark.svg';
 const Home = () => {
   return (
     <Section>
-      <TailwindDark className='mb-48 inline-block w-[38rem]' />
+      <div>
+        <TailwindDark className='mb-4 h-24' />
+        <p className='text-right font-space-mono text-xl'>
+          presented May 19th, 2022
+        </p>
+      </div>
     </Section>
   );
 };
